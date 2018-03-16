e4b4b1cf80660a730c836a3bb82255c448fe4a9c
Balazs Frey <balazs.frey@gmail.com>
2018-03-16 16:05:09 +0100

    Add routing to HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LocationPage } from '../location/location';
+import { GamePage } from '../game/game';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,16 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  favorite(){
+    this.navCtrl.parent.select(1);
+  }
+  location(){
+    this.navCtrl.push(LocationPage);
+  }
+  game(){
+    this.navCtrl.push(GamePage);
   }
 
 }
