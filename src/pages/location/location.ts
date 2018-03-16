09fa6664f2b11367469fb330f0def94dde83e824
Balazs Frey <balazs.frey@gmail.com>
2018-03-16 17:08:15 +0100

    Add google maps API

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -8,17 +8,39 @@ import { NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare var google: any;
+
 @Component({
   selector: 'page-location',
   templateUrl: 'location.html',
 })
 export class LocationPage {
 
+  @ViewChild('map') mapRef: ElementRef;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LocationPage');
+    this.showMap();
   }
 
+  showMap(){
+    const location = new google.maps.LatLng(47.472666, 19.059949);
+
+    const option = {
+      center: location,
+      zoom: 13
+    }
+
+    const map = new google.maps.Map(this.mapRef.nativeElement, option);
+
+    this.addMarker(location, map);
+  }
+  addMarker(position, map){
+    return new google.maps.Marker({
+      position,
+      map
+    })
+  }
 }
