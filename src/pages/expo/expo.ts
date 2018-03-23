5ebc3c82a8237484b7306086caa87f3dd44fe7bf
Balazs Frey <balazs.frey@gmail.com>
2018-03-23 02:20:15 +0100

    Style Expo page

--- src/pages/expo/expo.ts
+++ src/pages/expo/expo.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ExpoDetailsPage } from '../expo-details/expo-details';
+
 import { ExpoProvider } from '../../providers/expo/expo';
 
 @Component({
@@ -9,7 +11,7 @@ import { ExpoProvider } from '../../providers/expo/expo';
 })
 export class ExpoPage {
 
-  data: any = {};
+  data: any;
 
   constructor(public expoData: ExpoProvider, public navCtrl: NavController) {
     expoData.get().subscribe((data: any) => {
@@ -18,4 +20,7 @@ export class ExpoPage {
     });
   }
 
+  openExpo(ev) {
+    this.navCtrl.push(ExpoDetailsPage, { expo: ev });
+  }
 }
