0ecc7dbac5a047f017b741307bf9b6e43e18d4f6
bence04 <tothbence95@gmail.com>
2018-03-31 14:01:27 +0200

    fix onesignal bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-//import { OneSignal } from '@ionic-native/onesignal';
+import { OneSignal } from '@ionic-native/onesignal';
 
 import { PresentationProvider } from '../providers/presentation/presentation';
 import { UserProvider } from '../providers/user/user';
@@ -66,7 +66,7 @@ import { ExpoProvider } from '../providers/expo/expo';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    //OneSignal,
+    OneSignal,
     PresentationProvider,
     UserProvider,
     InformationProvider,
