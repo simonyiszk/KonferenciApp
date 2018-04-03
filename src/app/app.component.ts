7d6ef7f83d46f1e034fdb6e69ef8e8577d25f474
Balazs Frey <balazs.frey@gmail.com>
2018-04-03 22:40:32 +0200

    Fix back button's bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from "@ionic-native/splash-screen";
 import { OneSignal } from "@ionic-native/onesignal";
 import { HeaderColor } from '@ionic-native/header-color';
 
+import { HomePage } from '../pages/home/home';
 import { TabsPage } from "../pages/tabs/tabs";
 
 import { PresentationProvider } from "../providers/presentation/presentation";
@@ -33,11 +34,12 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      let nav = app.getActiveNav();
-      let activeView: ViewController = nav.getActive();
+
 
       if (platform.is("cordova")) {
         /*Onesignal id and firebase id*/
+        let nav = app.getActiveNav();
+
         oneSignal.startInit(
           "962eb6f9-c14b-4587-a025-1ff89486b119",
           "407073725617"
@@ -57,24 +59,29 @@ export class MyApp {
         });
 
         oneSignal.endInit();
-      }
 
-      statusBar.styleLightContent();
-      if (platform.is("android")) {
-        headerColor.tint("#42162C");
-        statusBar.backgroundColorByHexString("#42162C");
+        statusBar.styleLightContent();
+        if (platform.is("android")) {
+          headerColor.tint("#42162C");
+          statusBar.backgroundColorByHexString("#42162C");
+        }
       }
-      //Dunno if it works or not
+      
       //statusBar.styleDefault();
-
       splashScreen.hide();
 
       platform.registerBackButtonAction(() => {
+        let nav = app.getActiveNav();
+        let activeView: ViewController = nav.getActive();
+        console.log("ac", activeView);
         if (nav.canGoBack()) {
+          console.log("ac1");
           nav.pop();
-        } else if (activeView.component.name == "HomePage") {
+        } else if (activeView.instance instanceof HomePage) {
+          console.log("ac2");
           platform.exitApp();
         } else {
+          console.log("ac3");
           nav.parent.select(0);
         }
       });
