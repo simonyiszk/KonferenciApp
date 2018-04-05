9d5337866bca718b8a53dd10396c1d2fd1a61341
Balazs Frey <balazs.frey@gmail.com>
2018-04-06 00:04:28 +0200

    Wiring Periodic provider

--- src/providers/periodic/periodic.ts
+++ src/providers/periodic/periodic.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+import { PresentationProvider } from '../presentation/presentation';
+import { ExpoProvider } from '../expo/expo';
+import { PostProvider } from '../post/post';
 /*
   Generated class for the PeriodicProvider provider.
 
@@ -10,8 +13,29 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PeriodicProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello PeriodicProvider Provider');
+  lastDate: Date;
+
+  constructor(public postData: PostProvider, public expoData: ExpoProvider, public presData: PresentationProvider, public http: Http) {
+    this.lastDate = new Date("1997-06-16 00:00");
+    setTimeout(this.task.bind(this), 300000);
+  }
+
+  task() {
+    this.http.get(/*'http://gyromouse.net/weboldal/konferenciapi/last.php'*/'http://192.168.0.104:8080/favorites')
+      .map(res => new Date(res.text())).subscribe((data) => {
+        if (this.lastDate < data) {
+          this.presData.load().subscribe();
+          this.expoData.load().subscribe();
+         
+        }
+        this.postData.sendFavorites().catch((error) => {
+          console.log("Something went wrong:", error);
+        });
+      });
+    setTimeout(this.task.bind(this), 300000);
+  }
+  processData(data: any) {
+    return new Date(data.text());
   }
 
 }
